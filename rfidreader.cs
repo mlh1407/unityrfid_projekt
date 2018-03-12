f43f16e4c2e5b60a71b0a458816b8a765a3dc3c3
Martin Lilliendal Hansen <mlh1407@sebbo.local>
2018-03-12 16:38:48 +0100

    first draft
    hello world

---
+++ rfidreader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rfidreader : MonoBehaviour {
+
+	//public GUIText  gt;
+	public string gt;
+	public string[] ids;
+	public GameObject[] gos;
+	void Start()
+	{
+		//gt = GetComponent<GUIText>();
+	}
+
+	void Update()
+	{
+		foreach (char c in Input.inputString)
+		{
+			  if ((c == '\n') || (c == '\r')) // enter/return
+			{
+				print("User entered their name: " + gt);
+				gt = "";
+			}
+			else
+			{
+				gt += c;
+			}
+		}
+	}
+}
