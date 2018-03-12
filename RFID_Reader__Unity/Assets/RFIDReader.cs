7babbb3cb5e5ca9229b010a9bc3f0527d7208896
ChrK2017 <christian@familien-kirk.dk>
2018-03-12 17:12:23 +0100

    Unity Filerne
    Dette bør være alle Unity Filerne :-)

---
+++ RFID_Reader__Unity/Assets/RFIDReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RFIDReader : MonoBehaviour {
+
+	public string gt;
+
+	void Start()
+	{
+//gt = GetComponent<GUIText>();
+	}
+
+	void Update()
+	{
+		foreach (char c in Input.inputString)
+		{
+			if ((c == '\n') || (c == '\r')) // enter/return
+			{
+				print("UID: " + gt);
+				gt = "";
+			}
+			else
+			{
+				gt += c;
+			}
+		}
+	}
+}
