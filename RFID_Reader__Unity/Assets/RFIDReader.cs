84a3104babff6ab95650442fee09f946cb6e1bfc
ChrK2017 <christian@familien-kirk.dk>
2018-03-12 18:02:05 +0100

    Alt det vi lige har lavet

--- RFID_Reader__Unity/Assets/RFIDReader.cs
+++ RFID_Reader__Unity/Assets/RFIDReader.cs
@@ -7,10 +7,12 @@ public class RFIDReader : MonoBehaviour {
 	public string gt;
 	public string[] ids;
 	public GameObject[] gos;
+	public int[] states;
 
 	void Start()
 	{
 //gt = GetComponent<GUIText>();
+		off();
 	}
 
 	void Update()
@@ -20,12 +22,43 @@ public class RFIDReader : MonoBehaviour {
 			if ((c == '\n') || (c == '\r')) // enter/return
 			{
 				print("UID: " + gt);
+				if(isiton()){
+					
+					off ();
+				}
+				for (int i = 0; i < 2; i++) {
+					if(gt==ids[i] && states[i] == 0){
+						
+						gos[i].SetActive (true);
+						states[i]=1;
+ 
+					}
+				}
 				gt = "";
 			}
 			else
 			{
 				gt += c;
 			}
+
+		}
+	}
+	bool isiton(){
+		bool gogo = false;
+		for (int i = 0; i < 2; i++) {
+			if (states [i] == 0) {
+				gogo = false;
+			} else {
+				gogo = true;
+			}
+		}
+		return gogo;
+	}
+	void off() {
+		for (int i = 0; i < 2; i++) {
+			GameObject go = gos [i];
+			go.SetActive (false);
+			states[i]=0;
 		}
 	}
 }
